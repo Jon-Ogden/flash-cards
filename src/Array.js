439d273642b60a896c4312707c2e6b7b83c0c5b3
Andrew Owens <drew2447@gmail.com>
2022-05-26 14:55:11 -0600

    update

---
+++ src/Array.js
@@ -0,0 +1,15 @@
+const arrayOfquestions = () => {
+  const array = [
+    { id: 1, question: "What are two things you can never eat for breakfast?", answer:'Lunch and Dinner',correct:false },
+    { id: 2, question: "What is always coming but never arrives? ", anwser: "Tomorrow",correct:false },
+    { id: 3, question: "What gets wetter the more it dries?", anwser: "Towell",correct:false },
+    { id: 4, question: "What can be broken but never held?", anwser: "Promise",correct:false },
+    { id: 5, question: "What word is spelled incorrectly in every single dictionary?", anwser: "Incorrectly",correct:false },
+    { id: 6, question: "What is it that lives if it is fed, and dies if you give it a drink?", anwser: "Fire",correct:false },
+    { id: 7, question: "What never asks a question but gets answered all the time?", anwser: "Your Cellphone",correct:false },
+    { id: 8, question: "What word would you use to describe a man who does not have all his fingers on one hand?", anwser: "Normal, because people usually have half their fingers on one hand.",correct:false },
+    { id: 9, question: "What goes up but never ever comes down?", anwser: "Your age",correct:false },
+    { id: 10, question: "What can one catch that is not thrown?", anwser: "A Cold",correct:false },
+  ];
+}
+
